f200b3c4a4ce68cf781980397d79bd99d8fe6bce
Danilo Markicevic <danilo@pop-os.localdomain>
2022-07-05 16:13:57 +0200

    Main Layout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,15 @@
 export const Header = () => {
-  return <header>HEADER</header>
+  return (
+    <header className="flex px-2 py-3 bg-gray-200 justify-between">
+      <div className="flex justify-center flex-col space-y-1">
+        <div className="border-t-2 border-black w-4"></div>
+        <div className="border-t-2 border-black w-4"></div>
+        <div className="border-t-2 border-black w-4"></div>
+      </div>
+      <div className="flex">
+        <p>Avatar &nbsp;</p>
+        <p>Username</p>
+      </div>
+    </header>
+  )
 }
--- src/components/Layout/MainLayout.jsx
+++ src/components/Layout/MainLayout.jsx
@@ -1,11 +1,13 @@
 import { Header } from '@/components/Header'
 import { Footer } from '@/components/Footer'
+import { CompanyInfo } from '@/components/CompanyInfo'
 
 export const MainLayout = ({ children }) => {
   return (
-    <div className="flex flex-col min-h-screen overflow-hidden">
+    <div className="flex flex-col max-h-screen overflow-hidden">
       <Header />
-      <main className="flex-grow">{children}</main>
+      <CompanyInfo />
+      <main className="flex-grow overflow-auto p-4">{children}</main>
       <Footer />
     </div>
   )
