a19d529a92a22a33225f81f85962132e9d447689
vukstojanovic <82164509+vukstojanovic@users.noreply.github.com>
2022-07-18 20:54:00 +0200

    [implement-loader] Return dots

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,7 +1,44 @@
 export const Loader = () => {
   return (
     <>
-      <div className="w-32 h-32 relative"></div>
+      <div className="w-32 h-32 relative">
+        <div className="absolute top-0 left-0 transform rotate-[0deg] h-full w-full">
+          <div className="absolute w-6 h-6 bg-spinnerLoader top-0 left-0 rounded-full animate-spinLoader animation-delay-100"></div>
+        </div>
+        <div className="absolute top-0 left-0 transform rotate-[30deg] h-full w-full">
+          <div className="absolute w-6 h-6 bg-spinnerLoader top-0 left-0 rounded-full animate-spinLoader animation-delay-200"></div>
+        </div>
+        <div className="absolute top-0 left-0 transform rotate-[60deg] h-full w-full">
+          <div className="absolute w-6 h-6 bg-spinnerLoader top-0 left-0 rounded-full animate-spinLoader animation-delay-300"></div>
+        </div>
+        <div className="absolute top-0 left-0 transform rotate-[90deg] h-full w-full">
+          <div className="absolute w-6 h-6 bg-spinnerLoader top-0 left-0 rounded-full animate-spinLoader animation-delay-400"></div>
+        </div>
+        <div className="absolute top-0 left-0 transform rotate-[120deg] h-full w-full">
+          <div className="absolute w-6 h-6 bg-spinnerLoader top-0 left-0 rounded-full animate-spinLoader animation-delay-500"></div>
+        </div>
+        <div className="absolute top-0 left-0 transform rotate-[150deg] h-full w-full">
+          <div className="absolute w-6 h-6 bg-spinnerLoader top-0 left-0 rounded-full animate-spinLoader animation-delay-600"></div>
+        </div>
+        <div className="absolute top-0 left-0 transform rotate-[180deg] h-full w-full">
+          <div className="absolute w-6 h-6 bg-spinnerLoader top-0 left-0 rounded-full animate-spinLoader animation-delay-700"></div>
+        </div>
+        <div className="absolute top-0 left-0 transform rotate-[210deg] h-full w-full">
+          <div className="absolute w-6 h-6 bg-spinnerLoader top-0 left-0 rounded-full animate-spinLoader animation-delay-800"></div>
+        </div>
+        <div className="absolute top-0 left-0 transform rotate-[240deg] h-full w-full">
+          <div className="absolute w-6 h-6 bg-spinnerLoader top-0 left-0 rounded-full animate-spinLoader animation-delay-900"></div>
+        </div>
+        <div className="absolute top-0 left-0 transform rotate-[270deg] h-full w-full">
+          <div className="absolute w-6 h-6 bg-spinnerLoader top-0 left-0 rounded-full animate-spinLoader animation-delay-1000"></div>
+        </div>
+        <div className="absolute top-0 left-0 transform rotate-[300deg] h-full w-full">
+          <div className="absolute w-6 h-6 bg-spinnerLoader top-0 left-0 rounded-full animate-spinLoader animation-delay-1100"></div>
+        </div>
+        <div className="absolute top-0 left-0 transform rotate-[330deg] h-full w-full">
+          <div className="absolute w-6 h-6 bg-spinnerLoader top-0 left-0 rounded-full animate-spinLoader animation-delay-1200"></div>
+        </div>
+      </div>
     </>
   )
 }
